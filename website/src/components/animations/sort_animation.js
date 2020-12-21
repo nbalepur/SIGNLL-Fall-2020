1770c4feb4275003dd949eef22993ce3a3f9b34f
nishantbalepur <balepur2@illinois.edu>
2020-12-20 19:00:06 -0500

    refactored sorting

--- website/src/components/animations/sort_animation.js
+++ website/src/components/animations/sort_animation.js
@@ -1,30 +1,46 @@
 import React, { Component } from "react";
 
-import { wiki_sentence_sort } from "../../backend/storage/summarization_storage.js";
+import {
+  wiki_sentence_sort,
+  wiki_sentence_sorted,
+} from "../../backend/storage/summarization_storage.js";
 
-let key = 0;
-
-const Sentences = wiki_sentence_sort.map((sentence) => (
-  <div key={key}>
-    {sentence}
-    <p></p>
-  </div>
-));
+import FlipMove from "react-flip-move";
 
 class SortAnimation extends Component {
   state = {
     data: wiki_sentence_sort,
   };
 
-  componentDidMount() {}
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ data: wiki_sentence_sorted });
+    }, 2000);
+  };
+
+  createKey = (elem) => {
+    let keyStr = "";
+
+    for (let keyIndex = 0; keyIndex < 6; keyIndex++) {
+      keyStr += elem.props.children[keyIndex].props.children;
+    }
+
+    return keyStr;
+  };
 
   render() {
-    return this.state.data.map((sentence, key) => (
-      <div key={key}>
-        {sentence}
-        <p></p>
+    return (
+      <div>
+        <FlipMove>
+          {this.state.data.map((sentence) => (
+            <div key={this.createKey(sentence)}>
+              {sentence}
+              <p></p>
+            </div>
+          ))}
+        </FlipMove>
       </div>
-    ));
+    );
   }
 }
 
